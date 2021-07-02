e1d91c0e0f3c7c112a66d430fc873fb3e8bbe364
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 13:06:30 -0700

    add more data to the projects file

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,76 +4,77 @@ import './style.css';
 export default function Projects() {
     return (
         <div className='projects-section'>
-        <div class="container">
-            <div class="row">
-            <div class="section-title">
-                <h2 data-heading="my works">Projects</h2>
-            </div>
-            </div>
-            <div class="row project-items">
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow  ">
-                <div class="project-item-img">
-                    <img src="assets/images/what's-the-song.png" alt="what is the Song? app"/>
+            <div class="container">
+                <div class="row">
+                <div class="section-title">
+                    <h2 data-heading="my works">Projects</h2>
                 </div>
-                <p class="project-item-title">What's the Song? app</p>
-                <a href="https://asia-codeing.github.io/Whats-Song-is-That-the-app/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/Whats-Song-is-That-the-app.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
                 </div>
-            </div>
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow ">
-                <div class="project-item-img">
-                    <img src="assets/images/moi-me-ries.png" alt="moi-me-ries">
+                <div class="row project-items">
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow  ">
+                    <div class="project-item-img">
+                        <img src="assets/images/what's-the-song.png" alt="what is the Song? app"/>
+                    </div>
+                    <p class="project-item-title">What's the Song? app</p>
+                    <a href="https://asia-codeing.github.io/Whats-Song-is-That-the-app/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/Whats-Song-is-That-the-app.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-                <p class="project-item-title">MOI-ME-RIES</p>
-                <a href="https://blooming-ocean-65385.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/moi-me-ries.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow ">
+                    <div class="project-item-img">
+                        <img src="assets/images/moi-me-ries.png" alt="moi-me-ries"/>
+                    </div>
+                    <p class="project-item-title">MOI-ME-RIES</p>
+                    <a href="https://blooming-ocean-65385.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/moi-me-ries.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-            </div>
 
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow ">
-                <div class="project-item-img">
-                    <img src="assets/images/tech-blog.gif" alt="the-tech-blog">
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow ">
+                    <div class="project-item-img">
+                        <img src="assets/images/tech-blog.gif" alt="the-tech-blog">
+                    </div>
+                    <p class="project-item-title">The Tech Blog</p>
+                    <a href="https://mvc--tech--blog.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/MVC-Tech-Blog.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-                <p class="project-item-title">The Tech Blog</p>
-                <a href="https://mvc--tech--blog.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/MVC-Tech-Blog.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow ">
+                    <div class="project-item-img">
+                        <img src="assets/images/notes-taker.gif" alt="notes-taker"/>
+                    </div>
+                    <p class="project-item-title">Notes Taker</p>
+                    <a href="https://notes--application.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/Note-Taker.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-            </div>
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow ">
-                <div class="project-item-img">
-                    <img src="assets/images/notes-taker.gif" alt="notes-taker">
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow  ">
+                    <div class="project-item-img">
+                        <img src="assets/images/weather-app.png" alt="weather-dashboard"/>
+                    </div>
+                    <p class="project-item-title">Weather Dashboard app</p>
+                    <a href="https://asia-codeing.github.io/weather-dashboard/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/weather-dashboard.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-                <p class="project-item-title">Notes Taker</p>
-                <a href="https://notes--application.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/Note-Taker.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                <div class="project-item">
+                    <div class="project-item-inner outer-shadow hover-in-shadow ">
+                    <div class="project-item-img">
+                        <img src="assets/images/quiz-app.png" alt="quiz-challenge"/>
+                    </div>
+                    <p class="project-item-title">Quiz Challenge</p>
+                    <a href="https://asia-codeing.github.io/quiz-application/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/quiz-application.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    </div>
                 </div>
-            </div>
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow  ">
-                <div class="project-item-img">
-                    <img src="assets/images/weather-app.png" alt="weather-dashboard">
                 </div>
-                <p class="project-item-title">Weather Dashboard app</p>
-                <a href="https://asia-codeing.github.io/weather-dashboard/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/weather-dashboard.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
-                </div>
-            </div>
-            <div class="project-item">
-                <div class="project-item-inner outer-shadow hover-in-shadow ">
-                <div class="project-item-img">
-                    <img src="assets/images/quiz-app.png" alt="quiz-challenge"/>
-                </div>
-                <p class="project-item-title">Quiz Challenge</p>
-                <a href="https://asia-codeing.github.io/quiz-application/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                <a href="https://github.com/asia-codeing/quiz-application.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
-                </div>
-            </div>
-            </div>
-        </div>   
-    </div>
+            </div>   
+        </div>
     )
 }
+
