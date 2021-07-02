c669dabed0f9547e78aefb1f59aa232ed29d4f59
Asia Alnahi <asia.alius@gmail.com>
2021-07-01 21:35:08 -0700

    Created Header And Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2021. All Rights Reserved.</p>
+        </div>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Navbar from '../Navbar/Navbar';
+import './style.css';
+
+export default function Header() {
+    return (
+        <div className='header'>
+            <div className='header-wrapper'>
+                <div className='logo'> 
+                    <a href='#home'>Asia Alnahi.</a>
+                </div>
+            </div>
+        </div>
+    )
+}
