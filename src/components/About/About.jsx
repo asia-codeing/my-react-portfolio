5534020f4857a7cf0b530b41f513e123bdb820c6
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 12:44:27 -0700

    add styles to the about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './style.css';
-import image from '../../assets/images/profile2.png'
-import resume from '../../assets/my-resume.pdf'
+import image from '../../assets/images/profile2.png';
+import Resume from '../../assets/my-resume.pdf';
 
 export default function About() {
     return (
         <div className='about-section'>
             <div className="container">
-          <div className="row">
             <div className="section-title">
               <h2 data-heading="main info">About me</h2>
             </div>
@@ -19,13 +18,11 @@ export default function About() {
                </div>
             </div>
             <div className="info">
-              <P><span>My Name is Asia Alnahi</span> Full stack web developer with background in HTML, CSS,Bootstrap, GitHub, JavaScript, Node js, Sequelize, My SQL, Handlebars, Express, MVC, MongoDB and Heroku. With passion for creating web applications that are easy to use and responsive so they can  work on different screen sizes. Innovative problem-solver and development. Strengths in creativity, teamwork, and building projects from ideation to execution.
-              </P>
-              <a href={resume} target="_blank" className="outer-shadow hover-in-shadow btn" rel="noreferrer">Download Resume</a>
+              <p><span>My Name is Asia Alnahi</span> Full stack web developer with background in HTML, CSS,Bootstrap, GitHub, JavaScript, Node js, Sequelize, My SQL, Handlebars, Express, MVC, MongoDB and Heroku. With passion for creating web applications that are easy to use and responsive so they can  work on different screen sizes. Innovative problem-solver and development. Strengths in creativity, teamwork, and building projects from ideation to execution.
+              </p>
+              <a href={Resume} target="_blank" className="outer-shadow hover-in-shadow btn" rel="noreferrer">Download Resume</a>
             </div>
           </div>
-        </div>
-            
-        </div>
+        </div>        
     )
 }
