c97deb7db7a5d073499bdbec638c4bc2f798aac8
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 13:23:38 -0700

    Add more data for the projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,41 +4,40 @@ import './style.css';
 export default function Projects() {
     return (
         <div className='projects-section'>
-            <div class="container">
-                <div class="row">
-                <div class="section-title">
+            <div className="container">
+                <div className="section-title">
                     <h2 data-heading="my works">Projects</h2>
                 </div>
                 </div>
-                <div class="row project-items">
-                <div class="project-item">
-                    <div class="project-item-inner outer-shadow hover-in-shadow  ">
-                    <div class="project-item-img">
+                <div className="row project-items">
+                <div className="project-item">
+                    <div className="project-item-inner outer-shadow hover-in-shadow  ">
+                    <div className="project-item-img">
                         <img src="assets/images/what's-the-song.png" alt="what is the Song? app"/>
                     </div>
-                    <p class="project-item-title">What's the Song? app</p>
+                    <p className="project-item-title">What's the Song? app</p>
                     <a href="https://asia-codeing.github.io/Whats-Song-is-That-the-app/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
                     <a href="https://github.com/asia-codeing/Whats-Song-is-That-the-app.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
                     </div>
                 </div>
-                <div class="project-item">
-                    <div class="project-item-inner outer-shadow hover-in-shadow ">
-                    <div class="project-item-img">
+                <div classN="project-item">
+                    <div className="project-item-inner outer-shadow hover-in-shadow ">
+                    <div className="project-item-img">
                         <img src="assets/images/moi-me-ries.png" alt="moi-me-ries"/>
                     </div>
-                    <p class="project-item-title">MOI-ME-RIES</p>
-                    <a href="https://blooming-ocean-65385.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
-                    <a href="https://github.com/asia-codeing/moi-me-ries.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
+                    <p className="project-item-title">MOI-ME-RIES</p>
+                    <a href="https://blooming-ocean-65385.herokuapp.com/" target="_blank" className="outer-shadow hover-in-shadow btn">See it live</a>
+                    <a href="https://github.com/asia-codeing/moi-me-ries.git" target="_blank" className="outer-shadow hover-in-shadow btn">View Code</a>
                     </div>
                 </div>
 
-                <div class="project-item">
-                    <div class="project-item-inner outer-shadow hover-in-shadow ">
-                    <div class="project-item-img">
+                <div className="project-item">
+                    <div className="project-item-inner outer-shadow hover-in-shadow ">
+                    <div className="project-item-img">
                         <img src="assets/images/tech-blog.gif" alt="the-tech-blog">
                     </div>
-                    <p class="project-item-title">The Tech Blog</p>
-                    <a href="https://mvc--tech--blog.herokuapp.com/" target="_blank" class="outer-shadow hover-in-shadow btn">See it live</a>
+                    <p className="project-item-title">The Tech Blog</p>
+                    <a href="https://mvc--tech--blog.herokuapp.com/" target="_blank" className="outer-shadow hover-in-shadow btn">See it live</a>
                     <a href="https://github.com/asia-codeing/MVC-Tech-Blog.git" target="_blank" class="outer-shadow hover-in-shadow btn">View Code</a>
                     </div>
                 </div>
