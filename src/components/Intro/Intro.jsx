b1d016fbab87ba9d63a457c67b380a9a3a9d7ce4
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 11:14:29 -0700

    Add profile picture and give the home page a styles

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import "./style.css";
+import MyImage from '../../../public/assets/images/profile.png'
 
 export default function Inrto() {
     return (
-        <div className="intro-section" id="intro">
+        <div className="home-section" id="intro">
            <div class="container">
                <div class="row full-screen">
-                 <div class="home-text">
-                   <p>Hello</p>
-                   <h2>I'm Asia Alnahi</h2>
-                   <h1>Web Developer</h1>
-                 </div>
-                 <div class="home-img">
-                   <div class="img-box inner-shadow">
-                     <img src="./assets/images/profile.png" class="outer-shadow" alt="my-picture">
-                   </div>
-                 </div>
-               </div>
-             </div>
+                    <div class="home-text">
+                        <p>Hello</p>
+                        <h2>I'm Asia Alnahi</h2>
+                        <h1>Web Developer</h1>
+                    </div>
+                    <div class="home-img">
+                        <div class="img-box inner-shadow">
+                            <img src={MyImage} class="outer-shadow" alt="myprofile"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
