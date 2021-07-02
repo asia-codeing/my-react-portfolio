4d3d4eebedd955a08562a27c0af5f55f988af9aa
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 16:42:19 -0700

    changing some style in the nave bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom';
 export default function Nav() {
     return (
         <div className='navbar-items'>
-            <Link to='/'><p className='home'>Home</p></Link>
-            <Link to='/about'><p className='about'>About</p></Link>
-            <Link to='/projects'><p className='projects'>Projects</p></Link>
-            <Link to='/contact'><p className='contact'>Contact</p></Link>
+            <Link className='link' to='/'><p className='home'>Home</p></Link>
+            <Link className='link' to='/about'><p className='about'>About</p></Link>
+            <Link className='link' to='/projects'><p className='projects'>Projects</p></Link>
+            <Link className='link' to='/contact'><p className='contact'>Contact</p></Link>
         </div>
     )
 }
