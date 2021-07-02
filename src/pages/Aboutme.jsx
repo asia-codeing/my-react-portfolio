fb05cb9f2969b46dd98d11c9d2d78de5f4460e2d
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 15:47:17 -0700

    create about me page

---
+++ src/pages/Aboutme.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import About from '../components/About/About';
+
+export default function Aboutme() {
+    return (
+        <div>
+            <About/>
+        </div>
+    )
+}
