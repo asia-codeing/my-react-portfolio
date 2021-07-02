feb13b7c6e6cfb709393b4120441fae90b3f763c
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 15:52:10 -0700

    create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeIntro from '../components/Intro/Intro';
+
+export default function Home() {
+    return (
+        <div>
+            <HomeIntro/>
+        </div>
+    )
+}
