2bcf7e4849d42a399c5053bb53d84492e568091e
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 15:57:07 -0700

    Add the pages to App.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Header from '../src/components/Header/Header';
 import Footer from '../src/components/Footer/Footer';
-import Intro from '../src/components/Intro/Intro';
-import About from '../src/components/About/About';
-import Projects from '../src/components/Projects/Projects';
-import Contact from '../src/components/Contact/Contact';
+import Home from '../src/pages/Home';
+import AboutMe from '../src/pages/Aboutme';
+import MyProjects from '../src/pages/MyProjects';
+import ContactMe from '../src/pages/Contactme';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        <Intro/>
-        <About/>
-        <Projects/>
-        <Contact/>
+        <Home/>
+        <AboutMe/>
+        <MyProjects/>
+        <ContactMe/>
       <Footer/>
     </div>
   );
