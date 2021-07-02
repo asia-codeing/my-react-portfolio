02f7b9c90cc79059783c5a97f6ab0078132a0b63
Asia Alnahi <asia.alius@gmail.com>
2021-07-02 15:53:47 -0700

    Creating projects page

---
+++ src/pages/MyProjects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Projects from '../components/Projects/Projects';
+
+export default function MyProjects() {
+    return (
+        <div>
+            <Projects/>
+        </div>
+    )
+}
